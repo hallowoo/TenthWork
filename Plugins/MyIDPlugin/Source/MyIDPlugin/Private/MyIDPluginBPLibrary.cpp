79e6f723972a488cd1fd2a086cd721e4695dbf64
hallowoo <164675169+hallowoo@users.noreply.github.com>
2025-03-25 19:39:16 +0900

    add : comments

--- Plugins/MyIDPlugin/Source/MyIDPlugin/Private/MyIDPluginBPLibrary.cpp
+++ Plugins/MyIDPlugin/Source/MyIDPlugin/Private/MyIDPluginBPLibrary.cpp
@@ -17,13 +17,13 @@ float UMyIDPluginBPLibrary::MyIDPluginSampleFunction(float Param)
 
 void UMyIDPluginBPLibrary::SetUserName(const FString& NewName)
 {
-    if (UWorld* World = GEngine->GetCurrentPlayWorld())
+    if (UWorld* World = GEngine->GetCurrentPlayWorld())  // 현재 월드 가져오기
     {
-        if (UGameInstance* GameInstance = World->GetGameInstance())
+        if (UGameInstance* GameInstance = World->GetGameInstance())  // 가져온 월드와 연결된 게임 인스턴스 획득하기
         {
-            if (UMyIDSubsystem* Subsystem = GameInstance->GetSubsystem<UMyIDSubsystem>())
+            if (UMyIDSubsystem* Subsystem = GameInstance->GetSubsystem<UMyIDSubsystem>()) // 게임 인스턴스에서 게임 인스턴스 서브시스템 가져오기
             {
-                Subsystem->SetUserName(NewName);
+                Subsystem->SetUserName(NewName);    // 정보(유저 이름) 저장
             }
         }
     }
@@ -31,15 +31,15 @@ void UMyIDPluginBPLibrary::SetUserName(const FString& NewName)
 
 FString UMyIDPluginBPLibrary::GetUserName()
 {
-    if (UWorld* World = GEngine->GetCurrentPlayWorld())
+    if (UWorld* World = GEngine->GetCurrentPlayWorld()) // 현재 월드 가져오기
     {
-        if (UGameInstance* GameInstance = World->GetGameInstance())
+        if (UGameInstance* GameInstance = World->GetGameInstance()) // 가져온 월드와 연결된 게임 인스턴스 획득하기
         {
-            if (UMyIDSubsystem* Subsystem = GameInstance->GetSubsystem<UMyIDSubsystem>())
+            if (UMyIDSubsystem* Subsystem = GameInstance->GetSubsystem<UMyIDSubsystem>())   // 게임 인스턴스에서 게임 인스턴스 서브시스템 가져오기
             {
-                return Subsystem->GetUserName();
+                return Subsystem->GetUserName();     // 정보(유저 이름) 반환
             }
         }
     }
-    return TEXT("Unknown");
+    return TEXT("Unknown"); // 가져오기 실패 시 반환값
 }
